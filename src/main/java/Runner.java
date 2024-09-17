15a291d47d2eab88289f8059f6823970162c0832
gumburtu <aslanseval1986@gmail.com>
2024-09-17 18:59:29 +0100

    V3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,7 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Versiyon01");
         System.out.println("Versiyon02");
+        System.out.println("Versiyon03");
 
     }
 }
